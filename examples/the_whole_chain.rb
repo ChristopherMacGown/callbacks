9025c34b1dd755e71fbfe86f1879764d0ac2d24b
Christopher MacGown <chris@slicehost.com>
2010-02-16 22:27:59 -0600

    Less Contrived

--- examples/the_whole_chain.rb
+++ examples/the_whole_chain.rb
@@ -13,6 +13,26 @@ class BlockCallbacks
   after (:some_method) { p :after_some_method; true }
 end
 
+class LessContrivedExampleForLuckyTheTourist 
+  include Callbacks
+
+  attr_accessor :attributes
+
+  def initialize(attributes_hash = {})
+    @attributes = attributes_hash
+  end
+
+  def save!
+    File.open("/tmp/lucky", "w") do |file|
+      file.write Marshal.dump(self)
+    end
+  end
+
+  before :save! do |my|
+    true unless my.attributes.empty?
+  end
+end
+
 class MethodCallbacks
   include Callbacks
 
@@ -36,3 +56,5 @@ end
 
 p BlockCallbacks.new.some_method
 p MethodCallbacks.new.some_method
+p LessContrivedExampleForLuckyTheTourist.new.save!
+p LessContrivedExampleForLuckyTheTourist.new(:lucky => :the_tourist).save!
